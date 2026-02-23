8d6ac0a025490a90d0ef634be7f8f6c173a5ca25
Nisa <lnisa.yakarl@gmail.com>
2026-02-23 15:46:55 +0300

    Update Konum.java

--- src/main/java/com/vtys/proje/entity/Konum.java
+++ src/main/java/com/vtys/proje/entity/Konum.java
@@ -20,25 +20,17 @@ public class Konum {
     @Column(name = "konum_id")
     private Integer konumId;
 
-    /**
-     * Konumun görünen adı (İstanbul, Ankara Terminali vb.)
-     */
     @Column(name = "konum_adi", nullable = false, length = 100)
     private String konumAdi;
 
-    /**
-     * Şehir bilgisi ayrı tabloda tutulur (normalizasyon).
-     */
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "sehir_kodu", nullable = false)
     private Sehir sehir;
 
-    // Kalkış yapılan rotalar
     @OneToMany(mappedBy = "kalkisKonum")
     @JsonIgnore
     private List<Rota> kalkisRotalari;
 
-    // Varış yapılan rotalar
     @OneToMany(mappedBy = "varisKonum")
     @JsonIgnore
     private List<Rota> varisRotalari;
